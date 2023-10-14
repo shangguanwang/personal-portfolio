3635715d7456e5d3f15a9c0a20102b7aa0fb8b8f
Up Crown <wangshangguan@hotmail.com>
2023-10-13 23:42:05 -0500

    Add tooltip to project details link for Issue #11

--- src/views/BerryCart.jsx
+++ src/views/BerryCart.jsx
@@ -8,16 +8,18 @@ import githubsvg from "../assets/svgs/github.svg";
 import linksvg from "../assets/svgs/link.svg";
 import youtubesvg from "../assets/svgs/youtube.svg";
 import devtosvg from "../assets/svgs/devto.svg";
+import { Tooltip } from '@mui/material';
 
 
 const BerryCart = () => {
     const {techStack, liveUrl, githubUrl, youtubeUrl, blogUrl} = berrycartData;
   return (
     <div className="ml-2 lg:ml-6">
-        <h2 className='text-3xl font-bold pt-20 mb-3'>BerryCart</h2>
+        <h2 className='text-3xl font-bold pt-20 mb-3 tracking-wide'>BerryCart</h2>
         <div className='flex justify-between items-center max-w-[110ch]'>
         {/* External Links */}
         <div className='flex justify-between gap-2'>
+        <Tooltip title="website" placement="top">
         <a
                   href={liveUrl}
                   target="_blank"
@@ -25,6 +27,8 @@ const BerryCart = () => {
         >
             <img src={linksvg} className="w-5 h-5" alt="link icon" aria-label="BerryCart website" />    
         </a>
+        </Tooltip>
+        <Tooltip title="github" placement="top">
         <a
                   href={githubUrl}
                   target="_blank"
@@ -32,6 +36,8 @@ const BerryCart = () => {
         >
             <img src={githubsvg} className="w-5 h-5" alt="github icon" aria-label="BerryCart Github Repository" />    
         </a>
+        </Tooltip>
+        <Tooltip title="youtube demo" placement="top">
         <a
                   href={youtubeUrl}
                   target="_blank"
@@ -39,6 +45,8 @@ const BerryCart = () => {
         >
             <img src={youtubesvg} className="w-5 h-5" alt="youtube icon" aria-label="BerryCart Youtube Demo" />    
         </a>
+        </Tooltip>
+        <Tooltip title="blog post" placement="top">
         <a
                   href={blogUrl}
                   target="_blank"
@@ -46,6 +54,7 @@ const BerryCart = () => {
         >
             <img src={devtosvg} className="w-5 h-5" alt="devto blogging platform icon" aria-label="BerryCart Blog" />    
         </a>
+        </Tooltip>
         </div>
             {/*Tech Stack Badges */}
             <ul className="flex">
@@ -76,7 +85,7 @@ const BerryCart = () => {
             <img src={addItemImg} className='h-[70ch]' alt="Add Item page mockup" />
         </div>
         <section className='mb-10'>
-            <h2 className='text-3xl font-semibold mb-3'>Learnings</h2>
+            <h2 className='text-3xl font-semibold mb-3'>Outcomes</h2>
             <p className='text-2xl max-w-[75ch]'>
             This project not only sharpened my technical skills but also introduced me to various software engineering topics such as user journey design and accessibility. I also felt more comfortable dealing with merge conflicts and using git. I've learned the best practices for writing PR documentation and improving code readability, and most importantly how to collaborate in a software engineering team.
             </p>
